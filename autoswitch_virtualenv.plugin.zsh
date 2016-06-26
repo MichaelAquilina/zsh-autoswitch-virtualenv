aeabceae49e516a5d36da887d4771d20d1cfb50d
Michael Aquilina <michaelaquilina@gmail.com>
2016-06-26 22:53:43 +0100

    Add initial plugin version

---
+++ autoswitch_virtualenv.plugin.zsh
@@ -0,0 +1,24 @@
+precmd() { check_venv }
+
+
+function maybeworkon() {
+  if [ "$1" != "$(basename $VIRTUAL_ENV)" ]; then
+     printf "Switching virtualenv: $1  "
+     workon "$1"
+     # For some reason python --version writes to stderr
+     printf "[%s]\n" "$(python --version 2>&1)"
+  fi
+}
+
+# Automatically switch virtualenv when .venv file detected
+function check_venv()
+{
+    if [ "$PWD" != "$MYOLDPWD" ]; then
+        MYOLDPWD="$PWD"
+        if [[ -f ".venv" ]]; then
+           maybeworkon `cat .venv`
+        else
+           maybeworkon default3
+        fi
+    fi
+}
