2973523303f59d0030e0ec9ddb1d6a4499018ee2
Nick Cross <ncross@redhat.com>
2019-03-16 22:13:45 +0000

    Explicitly specify rm executable to avoid alias interference.

--- autoswitch_virtualenv.plugin.zsh
+++ autoswitch_virtualenv.plugin.zsh
@@ -167,8 +167,9 @@ function rmvenv()
         fi
 
         printf "Removing ${PURPLE}%s${NORMAL}...\n" "$venv_name"
-        rm -rf "$(_virtual_env_dir "$venv_name")"
-        rm ".venv"
+        # Using explicit paths to avoid any alias interference.
+        /bin/rm -rf "$(_virtual_env_dir "$venv_name")"
+        /bin/rm ".venv"
     else
         printf "No .venv file in the current directory!\n"
     fi
