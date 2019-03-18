c91fffca69eeb0f38cadee33ef9236a5c3ded5ff
Nick Cross <ncross@redhat.com>
2019-03-18 10:41:26 +0000

    Improve comment with cross-references

--- autoswitch_virtualenv.plugin.zsh
+++ autoswitch_virtualenv.plugin.zsh
@@ -167,7 +167,10 @@ function rmvenv()
         fi
 
         printf "Removing ${PURPLE}%s${NORMAL}...\n" "$venv_name"
-        # Using explicit paths to avoid any alias interference.
+        # Using explicit paths to avoid any alias/function interference.
+        # rm should always be found in this location according to
+        # https://refspecs.linuxfoundation.org/FHS_3.0/fhs/ch03s04.html
+        # https://www.freedesktop.org/wiki/Software/systemd/TheCaseForTheUsrMerge/
         /bin/rm -rf "$(_virtual_env_dir "$venv_name")"
         /bin/rm ".venv"
     else
